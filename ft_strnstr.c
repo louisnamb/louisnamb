e184d08d75ab1f7b4b85bb3a6948cd4c2edb6a34
louisnamb <63900979+louisnamb@users.noreply.github.com>
2022-01-21 14:27:08 +1030

    Create ft_strnstr.c

---
+++ ft_strnstr.c
@@ -0,0 +1,56 @@
+#include <string.h>
+#include <stdio.h>
+
+int		ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+char *strnstr(const char *haystack, const char *needle, size_t len)
+{
+    size_t i;
+    size_t j;
+
+    j = 0;
+    i = 0;
+    while (i < len)
+    {
+        if (haystack[i] == needle[j])
+        {
+            j++;
+        }
+        if (j == (size_t)ft_strlen(needle)])
+        {
+            return (haystack + (ft_strlen(haystack) - j));
+        }
+        i++;
+    }
+    // return vals
+    if (needle == NULL)
+    {
+        return (haystack);
+    }
+    else if (i == (size_t)ft_strlen(haystack))
+    {
+        return (NULL);
+    }
+    else
+    {
+        return (haystack[i]);
+    }
+}
+
+int main()
+{
+    const char *largestring = "Foo Bar Baz";
+    const char *smallstring = "Baz";
+    int i = 5;
+    printf("%s\n", largestring + 5);
+    printf("%d %s\n", i, strnstr(largestring, smallstring, i));
+    i++;
+}
